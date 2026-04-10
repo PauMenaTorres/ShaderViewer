6cd56090c52f9a57961f20605acfa9d4581d1925
Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>
2026-04-10 10:59:20 +0200

    add tree and more .obj

--- MyGLWidget.cpp
+++ MyGLWidget.cpp
@@ -147,14 +147,13 @@ void MyGLWidget::initializeGL()
     homer.init("Models3D/HomerProves.obj", ":/vertexModel.vert", ":/fragmentRed.frag");
     glm::mat4 tgHomer(1.0f);
     tgHomer = glm::translate(tgHomer, glm::vec3(0.3f, 0.0f, 0.0f));
-    tgHomer = glm::scale(tgHomer, glm::vec3(0.5f, 0.5f, 0.5f));
+    tgHomer = glm::scale(tgHomer, glm::vec3(0.5f));
     homer.modelTransform(tgHomer);
 
-    tree.init("Models3D/cow.obj", ":/vertexModel.vert", ":/fragmentGreen.frag");
+    tree.init("Models3D/tree.obj", ":/vertexModel.vert", ":/fragmentGreen.frag");
     glm::mat4 tgTree(1.0f);
-    tgTree = glm::rotate(tgTree, -(float)M_PI/2, glm::vec3(1.0, 0.0, 0.0));
     tgTree = glm::translate(tgTree, glm::vec3(-0.4f, 0.0f, 0.0f));
-    tgTree = glm::scale(tgTree, glm::vec3(0.5f, 0.5f, 0.5f));
+    tgTree = glm::scale(tgTree, glm::vec3(0.05f));
     tree.modelTransform(tgTree);
 
     SphereData defaultSphere;
