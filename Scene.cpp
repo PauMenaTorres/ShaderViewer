607d48b6bbac06400b4a3d5b525e56b79b641ec5
Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>
2026-04-17 11:02:16 +0200

    center tree

--- Scene.cpp
+++ Scene.cpp
@@ -11,23 +11,25 @@ void Scene::init()
     patricio->init("Models3D/Patricio.obj", ":/vertexModel.vert", ":/fragmentModel.frag");
     glm::mat4 tgPatricio(1.0f);
 
-    glm::vec3 center = patricio->getCenter();
+    glm::vec3 centerPatricio = patricio->getCenter();
 
     tgPatricio = glm::translate(tgPatricio, glm::vec3(0.3f, 0.0f, 0.0f));
     tgPatricio = glm::scale(tgPatricio, glm::vec3(0.2f));
-    tgPatricio = glm::translate(tgPatricio, -center);
+    tgPatricio = glm::translate(tgPatricio, -centerPatricio);
 
     patricio->modelTransform(tgPatricio);
 
     models.push_back(patricio);
 
-    tgPatricio = glm::translate(tgPatricio, -center);
     ModelOBJ* tree = new ModelOBJ;
     tree->init("Models3D/tree.obj", ":/vertexModel.vert", ":/fragmentModel.frag");
     glm::mat4 tgTree(1.0f);
-    tgTree = glm::translate(tgTree, glm::vec3(-0.8f, 0.0f, 0.0f));
+
+    glm::vec3 centerTree = tree->getCenter();
+
+    tgTree = glm::translate(tgTree, glm::vec3(-0.3f, 0.3f, -0.1f));
     tgTree = glm::scale(tgTree, glm::vec3(0.05f));
-    tgTree = glm::translate(tgTree, -center);
+    tgTree = glm::translate(tgTree, -centerTree);
 
     tree->modelTransform(tgTree);
 
